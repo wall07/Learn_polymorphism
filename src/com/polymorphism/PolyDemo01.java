61e6219843facdd9446bd7c8412e754145acf16f
wall07 <jiangliu07@163.com>
2017-05-10 22:57:04 +0800

    Upcasting
    多态，向上转型

---
+++ src/com/polymorphism/PolyDemo01.java
@@ -0,0 +1,30 @@
+package com.polymorphism;
+
+
+class A{
+    public void fun1(){
+        System.out.println("A--> public void fun1(){}");
+    }
+
+    public void fun2(){
+        this.fun1();
+    }
+}
+
+class B extends A{
+    public void fun1(){
+        System.out.println("B--> public void fun1(){}");
+    }
+
+    public void fun3(){
+        System.out.println("B--> public void fun3(){}");
+    }
+}
+
+public class PolyDemo01 {
+    public static void main(String[] args){
+        B b = new B();
+        A a = b;
+        a.fun1();
+    }
+}
